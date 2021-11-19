6ffdfcc620a0c5b8fdc7c039e41ca698285f0047
Take Care <takecare@pop-os.localdomain>
2021-11-19 23:23:57 +0100

    Add global flag

--- src/list.rs
+++ src/list.rs
@@ -1,9 +1,10 @@
 //! List all Todo lists in active Todo context
-use super::Context;
-use crate::parse::{parse_todo_list, ParsedTodoList};
+use crate::{
+    parse::{parse_todo_list, ParsedTodoList},
+    Configuration,
+};
 use clap::{crate_authors, App, Arg, ArgMatches};
 use log::{debug, trace};
-use regex::Regex;
 use std::fs::read_to_string;
 use walkdir::WalkDir;
 
@@ -17,7 +18,7 @@ pub fn list_command() -> App<'static, 'static> {
                 .short("l")
                 .long("label")
                 .value_name("LABEL")
-                .help("Filter by label")
+                .help("Filters by label")
                 .value_delimiter(",")
                 .takes_value(true),
         )
@@ -25,25 +26,34 @@ pub fn list_command() -> App<'static, 'static> {
             Arg::with_name("short")
                 .short("s")
                 .long("short")
-                .help("one line"),
+                .help("Displays one line summary"),
         )
         .arg(
             Arg::with_name("all")
                 .short("a")
                 .long("all")
-                .help("show all Todo lists"),
+                .help("Shows all Todo lists"),
         )
         .arg(
             Arg::with_name("done")
                 .short("d")
                 .long("done")
-                .help("Show only fully completed task lists"),
+                .help("Shows only fully completed task lists"),
+        )
+        .arg(
+            Arg::with_name("global")
+                .short("g")
+                .long("global")
+                .help("Lists Todo lists from all contexts"),
         )
 }
 
 /// Lists Todo lists from Todo context while filtering by label and whether or not the task list is
 /// completed
-pub fn list_command_process(args: &ArgMatches, ctx: &Context) -> Result<(), std::io::Error> {
+pub fn list_command_process(
+    args: &ArgMatches,
+    config: &Configuration,
+) -> Result<(), std::io::Error> {
     trace!("list subcommand");
 
     let labels = args
@@ -55,54 +65,47 @@ pub fn list_command_process(args: &ArgMatches, ctx: &Context) -> Result<(), std:
     let short = args.is_present("short");
     let all = args.is_present("all");
     let done = args.is_present("done");
+    let global = args.is_present("global");
 
-    let label_re: Regex = Regex::new(r"LABEL=(.*)\n---").unwrap();
-
-    println!("Todo lists from {}", ctx.folder_location);
-    for entry in WalkDir::new(ctx.folder_location.as_str()) {
-        let entry = entry.unwrap();
-        if entry.file_type().is_dir() {
-            // first entry is the todo folder
+    for ctx in &config.ctxs {
+        if !global && ctx.name != config.active_ctx_name {
             continue;
         }
-        let filepath = entry.path().to_str().unwrap();
-        debug!("todo: {}", filepath);
-        debug!("short: {}", short);
-        match read_to_string(filepath) {
-            Ok(content) => {
-                let todo = parse_todo_list(&content).unwrap();
-                let label_matches = label_re.captures(content.as_str()).unwrap();
-                if label_matches.len() == 1 {
-                    eprintln!("Cannot parse {} labels", filepath);
-                    std::process::exit(1);
-                }
 
-                let file_labels = label_matches
-                    .get(1)
-                    .unwrap()
-                    .as_str()
-                    .split(",")
-                    .map(|s| s.to_string())
-                    .collect::<Vec<String>>();
-                if labels.iter().all(|l| file_labels.iter().any(|fl| fl == l)) {
-                    let is_done = todo.tasks_are_all_done();
-                    // so XOR is a thing: https://doc.rust-lang.org/reference/types/boolean.html#logical-xor
-                    if !all && (is_done ^ done) {
-                        continue;
-                    }
-
-                    if short {
-                        print_short(content.as_str());
-                    } else {
-                        println!("{}", content);
-                    }
-                }
+        println!("Todo lists from {}", ctx.folder_location);
+        for entry in WalkDir::new(ctx.folder_location.as_str()) {
+            let entry = entry.unwrap();
+            if entry.file_type().is_dir() {
+                // first entry is the todo folder
+                continue;
             }
-            Err(error) => panic!(
-                "Cannot open {}, error: {}",
-                entry.path().to_str().unwrap(),
-                error
-            ),
+            let filepath = entry.path().to_str().unwrap();
+            debug!("todo: {}", filepath);
+            debug!("short: {}", short);
+            match read_to_string(filepath) {
+                Ok(content) => {
+                    let todo = parse_todo_list(&content)?;
+
+                    if labels.iter().all(|l| todo.labels.iter().any(|fl| fl == l)) {
+                        let is_done = todo.tasks_are_all_done();
+                        // so XOR is a thing: https://doc.rust-lang.org/reference/types/boolean.html#logical-xor
+                        if !all && (is_done ^ done) {
+                            continue;
+                        }
+
+                        if short {
+                            print_short(content.as_str());
+                        } else {
+                            println!("{}", content);
+                        }
+                    }
+                }
+                Err(error) => panic!(
+                    "Cannot open {}, error: {}",
+                    entry.path().to_str().unwrap(),
+                    error
+                ),
+            }
         }
     }
 
